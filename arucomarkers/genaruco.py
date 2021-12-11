04b2ffcbfb2e77946f6763b0a0c3ac5ee33336e2
bparento <sp3643@drexel.edu>
2021-12-11 17:47:19 -0500

    Im doing shit

--- arucomarkers/genaruco.py
+++ arucomarkers/genaruco.py
@@ -52,8 +52,8 @@ print(ARUCO_DICT[args["type"]])
 # lets gen some markers
 print("[INFO] generating AruCo tag type '{}' with ID '{}'".format(args["type"], args["id"]))
 
-tag = np.zeros((300, 300, 1), dtype="uint8")
-cv2.aruco.drawMarker(arucodict, args["id"], 300, tag, 1)
+tag = np.zeros((1200, 1200, 1), dtype="uint8")
+cv2.aruco.drawMarker(arucodict, args["id"], 1200, tag, 1)
 
 cv2.imwrite(args["output"], tag)
 cv2.imshow("ArUCo Tag", tag)
